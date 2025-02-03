6d7d1efcc0829bf7ce73f533f773ba2b93c48a37
medfatnasii277 <mohamedfatnassi277@gmail.com>
2025-02-03 19:10:16 +0100

    yo

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
 
+// Routes
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.json({ response: "Hello From Thetips4you" });
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.json({ response: "Hello World" });
 });
+
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.json({ response: "Great!, It works!" });
 });
-app.listen(process.env.PORT || 4000);
+
+// Only start the server if this file is run directly (not in tests)
+if (require.main === module) {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log('Server running on port ' + (process.env.PORT || 4000));
+    });
+}
+
+// Export the app for testing
 module.exports = app;
+
